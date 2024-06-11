832fc21d1a0726d1bd395097495c7c878c3b9296
toliadriapak <toliadriapak@gmail.com>
2024-06-11 20:31:41 +0300

    Added coments

--- Text editor multi windows/EditorForm.cs
+++ Text editor multi windows/EditorForm.cs
@@ -7,66 +7,71 @@ namespace TextEditor
 {
     public partial class EditorForm : Form
     {
+        // Конструктор редактора з параметром для культури інтерфейсу
         public EditorForm(CultureInfo culture)
         {
-            InitializeComponent();
+            InitializeComponent(); // Ініціалізація компонентів форми
             // Apply culture-specific settings if necessary
 
             // Додаємо обробники подій у конструкторі або іншому методі ініціалізації
-            this.openMenuItem.Click += new System.EventHandler(this.OpenFile);
-            this.saveMenuItem.Click += new System.EventHandler(this.SaveFile);
-            this.alignLeftMenuItem.Click += new System.EventHandler((s, e) => richTextBox.SelectionAlignment = HorizontalAlignment.Left);
-            this.alignCenterMenuItem.Click += new System.EventHandler((s, e) => richTextBox.SelectionAlignment = HorizontalAlignment.Center);
-            this.alignRightMenuItem.Click += new System.EventHandler((s, e) => richTextBox.SelectionAlignment = HorizontalAlignment.Right);
-            this.fontMenuItem.Click += new System.EventHandler(this.ChangeFont);
-            this.insertImageMenuItem.Click += new System.EventHandler(this.InsertImage);
+            this.openMenuItem.Click += new System.EventHandler(this.OpenFile); // Відкриття файлу
+            this.saveMenuItem.Click += new System.EventHandler(this.SaveFile); // Збереження файлу
+            this.alignLeftMenuItem.Click += new System.EventHandler((s, e) => richTextBox.SelectionAlignment = HorizontalAlignment.Left); // Вирівнювання тексту по лівому краю
+            this.alignCenterMenuItem.Click += new System.EventHandler((s, e) => richTextBox.SelectionAlignment = HorizontalAlignment.Center); // Вирівнювання тексту по центру
+            this.alignRightMenuItem.Click += new System.EventHandler((s, e) => richTextBox.SelectionAlignment = HorizontalAlignment.Right); // Вирівнювання тексту по правому краю
+            this.fontMenuItem.Click += new System.EventHandler(this.ChangeFont); // Зміна шрифту
+            this.insertImageMenuItem.Click += new System.EventHandler(this.InsertImage); // Вставка зображення
         }
 
+        // Метод для відкриття файлу
         private void OpenFile(object sender, EventArgs e)
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
-                openFileDialog.Filter = "RTF files|*.rtf|Text files|*.txt|All files|*.*";
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                openFileDialog.Filter = "RTF files|*.rtf|Text files|*.txt|All files|*.*"; // Фільтр типів файлів для відкриття
+                if (openFileDialog.ShowDialog() == DialogResult.OK) // Відображення діалогового вікна для відкриття файлу
                 {
-                    richTextBox.LoadFile(openFileDialog.FileName);
+                    richTextBox.LoadFile(openFileDialog.FileName); // Завантаження вибраного файлу у RichTextBox
                 }
             }
         }
 
+        // Метод для збереження файлу
         private void SaveFile(object sender, EventArgs e)
         {
             using (SaveFileDialog saveFileDialog = new SaveFileDialog())
             {
-                saveFileDialog.Filter = "RTF files|*.rtf|Text files|*.txt|All files|*.*";
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                saveFileDialog.Filter = "RTF files|*.rtf|Text files|*.txt|All files|*.*"; // Фільтр типів файлів для збереження
+                if (saveFileDialog.ShowDialog() == DialogResult.OK) // Відображення діалогового вікна для збереження файлу
                 {
-                    richTextBox.SaveFile(saveFileDialog.FileName);
+                    richTextBox.SaveFile(saveFileDialog.FileName); // Збереження вмісту RichTextBox у вибраний файл
                 }
             }
         }
 
+        // Метод для зміни шрифту
         private void ChangeFont(object sender, EventArgs e)
         {
             using (FontDialog fontDialog = new FontDialog())
             {
-                if (fontDialog.ShowDialog() == DialogResult.OK)
+                if (fontDialog.ShowDialog() == DialogResult.OK) // Відображення діалогового вікна для вибору шрифту
                 {
-                    richTextBox.SelectionFont = fontDialog.Font;
+                    richTextBox.SelectionFont = fontDialog.Font; // Застосування вибраного шрифту до виділеного тексту
                 }
             }
         }
 
+        // Метод для вставки зображення
         private void InsertImage(object sender, EventArgs e)
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
-                openFileDialog.Filter = "Image files|*.bmp;*.jpg;*.jpeg;*.png|All files|*.*";
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                openFileDialog.Filter = "Image files|*.bmp;*.jpg;*.jpeg;*.png|All files|*.*"; // Фільтр типів файлів для вставки зображення
+                if (openFileDialog.ShowDialog() == DialogResult.OK) // Відображення діалогового вікна для вибору зображення
                 {
-                    Image img = Image.FromFile(openFileDialog.FileName);
-                    Clipboard.SetImage(img);
-                    richTextBox.Paste();
+                    Image img = Image.FromFile(openFileDialog.FileName); // Завантаження вибраного зображення
+                    Clipboard.SetImage(img); // Копіювання зображення у буфер обміну
+                    richTextBox.Paste(); // Вставка зображення з буфера обміну у RichTextBox
                 }
             }
         }
--- Text editor multi windows/Form1.cs
+++ Text editor multi windows/Form1.cs
@@ -7,38 +7,44 @@ namespace TextEditor
 {
     public partial class MainForm : Form
     {
+        // Декларація ресурсного менеджера та культури для багатомовного інтерфейсу
         private ResourceManager resManager;
         private CultureInfo currentCulture;
 
+        // Конструктор головної форми
         public MainForm()
         {
-            InitializeComponent();
-            InitializeLocalization();
-            this.WindowState = FormWindowState.Maximized;
-            // Додаємо обробники подій у конструкторі або іншому методі ініціалізації
-            this.newMenuItem.Click += new System.EventHandler(this.NewEditorWindow);
-            this.exitMenuItem.Click += new System.EventHandler((s, e) => this.Close());
-            this.englishMenuItem.Click += new System.EventHandler((s, e) => ChangeLanguage("en"));
-            this.ukrainianMenuItem.Click += new System.EventHandler((s, e) => ChangeLanguage("uk"));
+            InitializeComponent(); // Ініціалізація компонентів форми
+            InitializeLocalization(); // Ініціалізація локалізації
+            this.WindowState = FormWindowState.Maximized; // Встановлення повноекранного режиму при запуску
+
+            // Прив'язка подій до елементів меню
+            this.newMenuItem.Click += new System.EventHandler(this.NewEditorWindow); // Відкриття нового вікна редактора
+            this.exitMenuItem.Click += new System.EventHandler((s, e) => this.Close()); // Закриття програми
+            this.englishMenuItem.Click += new System.EventHandler((s, e) => ChangeLanguage("en")); // Зміна мови на англійську
+            this.ukrainianMenuItem.Click += new System.EventHandler((s, e) => ChangeLanguage("uk")); // Зміна мови на українську
         }
 
+        // Метод для ініціалізації локалізації
         private void InitializeLocalization()
         {
-            resManager = new ResourceManager("TextEditor.Resources", typeof(MainForm).Assembly);
-            currentCulture = CultureInfo.CurrentCulture;
+            resManager = new ResourceManager("TextEditor.Resources", typeof(MainForm).Assembly); // Ініціалізація ресурсного менеджера
+            currentCulture = CultureInfo.CurrentCulture; // Встановлення поточної культури
         }
 
+        // Метод для зміни мови інтерфейсу
         private void ChangeLanguage(string lang)
         {
-            currentCulture = new CultureInfo(lang);
-            // Change UI text to the selected language if needed
+            currentCulture = new CultureInfo(lang); // Зміна поточної культури на вибрану
+            // Тут можна додати код для зміни тексту інтерфейсу на обрану мову
         }
 
+        // Метод для створення нового вікна редактора
         private void NewEditorWindow(object sender, EventArgs e)
         {
-            EditorForm editorForm = new EditorForm(currentCulture);
-            editorForm.MdiParent = this;
-            editorForm.Show();
+            EditorForm editorForm = new EditorForm(currentCulture); // Створення нового вікна редактора з поточною культурою
+            editorForm.MdiParent = this; // Встановлення батьківського вікна
+            editorForm.Show(); // Показ вікна редактора
         }
     }
 }
